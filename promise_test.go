a659e95589f4c36980477ac4c702ec341dd10bd8
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2010-10-24 23:14:29 -0400

    Added basic testing.

---
+++ promise_test.go
@@ -0,0 +1,33 @@
+package promise
+
+import (
+	"testing"
+)
+
+func TestSanity(t *testing.T) {
+	if 0 != 0 {
+		t.Errorf("0 is not equal to 0, so the universe is meaningless")
+	}
+}
+
+func TestEvaluation(t *testing.T) {
+	f1 := func() interface{} {
+		return 3
+	}
+	p := Delay(f1)
+	if p.done != false {
+		t.Errorf("p.done should be %s, but it was %s", false, p.done)
+	}
+	if p.value != nil {
+		t.Errorf("p.value should be %s, but it was %s", nil, p.value)
+	}
+	if val := p.Force(); val != 3 {
+		t.Errorf("p.Force should have returned %s, got %s", 3, val)
+	}
+	if p.done != true {
+		t.Errorf("p.done should be %s, but it was %s", true, p.done)
+	}
+	if p.value != 3 {
+		t.Errorf("p.value should be %s, but it was %s", 3, p.value)
+	}
+}
