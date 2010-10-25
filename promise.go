d23bb929b060645e8d157a1ebfe807fad77ac76c
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2010-10-24 23:18:07 -0400

    Added inline documentation.

--- promise.go
+++ promise.go
@@ -6,16 +6,22 @@
 //delayed evaluation
 package promise
 
+//A Promise contains a function, which will be evaluated
+//at most once, whenever Force() is called.
 type Promise struct {
 	f     func() interface{}
 	value interface{}
 	done  bool
 }
 
+//Delay(f) takes a function and returns a Promise, which will
+//evaluate f if and when necessary.
 func Delay(f func() interface{}) *Promise {
 	return &Promise{f: f}
 }
 
+//p.Force() evaluates p only if it has never been evaluated
+//before--otherwise it returns the cached result.
 func (p *Promise) Force() interface{} {
 	if !p.done {
 		p.value = p.f()
