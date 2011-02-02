35e127b8b184a0a6729148735dcbacbfa457a88a
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2011-02-01 20:35:09 -0500

    Rewrote to move burden to Force.
    Now Delay() has no reflection in it.

--- promise.go
+++ promise.go
@@ -14,8 +14,8 @@ import (
 //A Promise contains a function, which will be evaluated
 //at most once, whenever Force() is called.
 type Promise struct {
-	f      *reflect.FuncValue
-	args   []reflect.Value
+	f      interface{}
+	args   []interface{}
 	values []interface{}
 	once   sync.Once
 }
@@ -24,22 +24,22 @@ type Promise struct {
 //evaluate f if and when necessary. f WILL ONLY BE RUN ONCE,
 //so take this into account if it has side-effects.
 func Delay(f interface{}, args ...interface{}) *Promise {
-	p := new(Promise)
-	p.f = reflect.NewValue(f).(*reflect.FuncValue)
-	p.args = make([]reflect.Value, len(args))
-	for i, arg := range args {
-		p.args[i] = reflect.NewValue(arg)
-	}
-	p.values = make([]interface{}, p.f.Type().(*reflect.FuncType).NumOut())
-	return p
+	return &Promise{f: f, args: args}
 }
 
 //p.Force() evaluates p only if it has never been evaluated
 //before--otherwise it returns the cached result.
 func (p *Promise) Force() []interface{} {
 	p.once.Do(func() {
-		for i, v := range p.f.Call(args) {
-			values[i] = v.Interface()
+		fV := reflect.NewValue(p.f).(*reflect.FuncValue)
+		in := make([]reflect.Value, len(p.args))
+		for i, v := range p.args {
+			in[i] = reflect.NewValue(v)
+		}
+		out := fV.Call(in)
+		p.values = make([]interface{}, len(out))
+		for i, v := range out {
+			p.values[i] = v.Interface()
 		}
 	})
 	return p.values
