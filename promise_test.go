d7d9319c0945eed4497771d000d3a3d2fc86e48c
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2011-02-01 20:50:30 -0500

    New, slightly more sensible tests.

--- promise_test.go
+++ promise_test.go
@@ -2,26 +2,37 @@ package promise
 
 import (
 	"testing"
+	"time"
 )
 
 func TestSanity(t *testing.T) {
 	if 0 != 0 {
-		t.Errorf("0 is not equal to 0, so the universe is meaningless")
+		t.Error("0 is not equal to 0, so the universe is meaningless.")
+	}
+	t1 := time.Nanoseconds()
+	time.Sleep(5e8)
+	t2 := time.Nanoseconds()
+	if t1 == t2 {
+		t.Error("Time didn't change during sleep.")
+	}
+	if t1 > t2 {
+		t.Error("Time went backwards during sleep.")
 	}
 }
 
 func TestEvaluation(t *testing.T) {
-	f1 := func() interface{} {
-		return 3
+	p1 := Delay(time.Nanoseconds)
+	p2 := Delay(time.Nanoseconds)
+	p2V := p2.Force()[0].(int64)
+	time.Sleep(5e8)
+	p1V := p1.Force()[0].(int64)
+	if p1V == p2V {
+		t.Error("Promises conflated.")
 	}
-	p := Delay(f1)
-	if p.value != nil {
-		t.Errorf("p.value should be %s, but it was %s", nil, p.value)
+	if p1V < p2V {
+		t.Error("Functions were evaluated at Delay time rather than Force time.")
 	}
-	if val := p.Force(); val != 3 {
-		t.Errorf("p.Force should have returned %s, got %s", 3, val)
-	}
-	if p.value != 3 {
-		t.Errorf("p.value should be %s, but it was %s", 3, p.value)
+	if p2.Force()[0].(int64) != p2V {
+		t.Error("Value not memoized.")
 	}
 }
